6221638beb47fab86fe76f09f482e7103b019cda
Awesome User <61346229+saspav@users.noreply.github.com>
2023-07-22 20:56:37 +0300

    add file

---
+++ mapper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    words = line.replace('"', ' ').replace(',', ' ').split()
+    for word in words:
+        print(f'{word}\t1')
