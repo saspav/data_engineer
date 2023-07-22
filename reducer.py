95955b5852a812ff86fb82a7f240460e27f12aa2
Awesome User <61346229+saspav@users.noreply.github.com>
2023-07-22 22:13:12 +0300

    add file

---
+++ reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+from collections import defaultdict
+
+word_counts = defaultdict(int)
+
+for line in sys.stdin:
+    word, count = line.split('\t', 1)
+    try:
+        word_counts[word] += int(count)
+    except ValueError:
+        continue
+
+for word, count in word_counts.items():
+    print(f'{word} {count}')
